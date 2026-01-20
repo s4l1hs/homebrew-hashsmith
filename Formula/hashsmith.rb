ae09ccb2dd9e21a68d2621b3819a86247cad4e44
salihsefer36 <sefersalih017@gmail.com>
2026-01-20 13:59:40 +0300

    Update hashsmith.rb

--- Formula/hashsmith.rb
+++ Formula/hashsmith.rb
@@ -4,7 +4,7 @@ class Hashsmith < Formula
   desc "Swiss Army knife for encoding, hashing, and cracking"
   homepage "https://github.com/s4l1hs/Hashsmith"
   url "https://github.com/s4l1hs/Hashsmith/archive/refs/tags/v1.0.1.tar.gz"
-  sha256 "d5558cd419c8d46bdc958064cb97f963d1ea793866414c025906ec15033512ed"
+  sha256 "85cf3faa928319ec043ecdea0925eb7a5973b1794d47ca5ee06dc9f7b4d36c58"
 
   depends_on "python@3.10"
 
